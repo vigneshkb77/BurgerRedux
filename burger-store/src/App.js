43c883d7a16533affc6256910dc40107c2a3d431
Vignesh K B <vigneshbala753@gmail.com>
2018-09-15 23:04:40 +0530

    Layout implemented

--- burger-store/src/App.js
+++ burger-store/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layouts/layout';
 class App extends Component {
   render() {
     return (
       <div className="App">
         React-
         <h1>September 15</h1>
+        <Layout />
       </div>
     );
   }
